941e60d24b8b3ac882724f8270d46f16ce7137b1
Nathan Steed <steedn@outlook.com>
2020-09-08 07:03:51 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,8 +78,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   movie = Movie.create(title: "Wat?")
+  movie = Movie.find_by(title: "Wat?")
   movie.update(title: "Wat, huh?")
-  movie.save
 end
 
 def can_update_multiple_items_at_once
