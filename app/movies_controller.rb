75dd00a73a99ab1d0f58d9b9399ab11f66e8b19f
Nathan Steed <steedn@outlook.com>
2020-09-08 06:59:19 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,9 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save
   end
 end
 
