c45a67625ab7e23916efc82bde957e33906c8aee
Nathan Steed <steedn@outlook.com>
2020-09-08 06:56:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,9 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = attributes
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = __)
